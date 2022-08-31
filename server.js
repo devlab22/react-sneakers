fad4a2bf3271acd6529b5dec4bef2843c8ce3b5d
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2022-08-31 11:44:10 +0200

    final

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+PORT = 9000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  //console.log('server on port 9000')
+  console.log(`server on port ${PORT}`)
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(PORT);
